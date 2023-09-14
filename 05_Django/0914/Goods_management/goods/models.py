2ab1ab4064804a641cbb07edc19a16d59fd0f48a
조혜원 <a01051910573@gmail.com>
2023-09-14 17:39:40 +0900

    0914

---
+++ 05_Django/0914/Goods_management/goods/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    name = models.TextField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10,  decimal_places=1)
+    is_published = models.BooleanField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
