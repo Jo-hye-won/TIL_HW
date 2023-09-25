bf6737669f1dc21ee27c1afc08a6996d39436b24
Seungjip Lee <edward626kr@naver.com>
2023-09-25 11:53:27 +0900

    test

---
+++ 05_Django/0925/exam/Algo1_부울경_1반_조혜원.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('algo1_sample_in.txt')
+
+hex_to_dec = {hex(val)[2:].upper():val for val in range(16)}
+
+T = int(input())
+for tc in range(1, T+1):
+    N = input()
+    data = input()
+    key = input()
+    result = ''
+    for char in data:
+        result += hex_to_dec[char] ^ hex_to_dec[key][2:].upper()
+    print(result)
